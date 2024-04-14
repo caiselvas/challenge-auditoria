49668d518dd235a31d6516f42f0fee62e1773f79
caiselvass <caiselvassala@gmail.com>
2024-04-14 21:03:01 +0200

    MOD column names

--- clean_raw_dataset.py
+++ clean_raw_dataset.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-info_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Inf.')
-accounting_info_1_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='info_comptable_1')
-accounting_info_2_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='info_comptable_2')
-sales_2023_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Vendes 2023')
-sales_2022_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Vendes 2022')
-catalog_prices_2023_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Preus de venda catàleg 2023')
-last_exit_entry_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Darrera entrada-sortida')
-stock_31_12_2023_df = pd.read_excel('data/inventory_data.xlsx', sheet_name='Stock 31.12.23')
+info_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Inf.')
+accounting_info_1_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='info_comptable_1')
+accounting_info_2_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='info_comptable_2')
+sales_2023_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Vendes 2023')
+sales_2022_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Vendes 2022')
+catalog_prices_2023_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Preus de venda catàleg 2023')
+last_exit_entry_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Darrera entrada-sortida')
+stock_31_12_2023_df = pd.read_excel('data/inventory_data_semi_raw.xlsx', sheet_name='Stock 31.12.23')
 
 # Strip all column names
 for col in info_df.columns:
@@ -46,8 +46,8 @@ stock_31_12_2023_df.drop(columns=['Mon.', 'Mon..1', 'UMB'], inplace=True)
 # Rename columns
 accounting_info_1_df.rename(columns={'Núm compte': 'num_compte', 'Etiqueta': 'etiqueta', 'Final 2023': 'final_2023', 'Final 2022': 'final_2022'}, inplace=True)
 accounting_info_2_df.rename(columns={'Núm compte': 'num_compte', 'Etiqueta': 'etiqueta', 'Exercici 2023': 'exercici_2023', 'Exercici 2022': 'exercici_2022'}, inplace=True)
-sales_2023_df.rename(columns={'Material (Referencia)': 'material', 'Quantitat facturada': 'quantitat', 'Ventes (sense IVA)': 'ventes'}, inplace=True)
-sales_2022_df.rename(columns={'Material (Referencia)': 'material', 'Quantitat facturada': 'quantitat', 'Ventes (sense IVA)': 'ventes'}, inplace=True)
+sales_2023_df.rename(columns={'Material (Referencia)': 'material', 'Quantitat facturada': 'quantitat', 'Ventes (sense IVA)': 'vendes'}, inplace=True)
+sales_2022_df.rename(columns={'Material (Referencia)': 'material', 'Quantitat facturada': 'quantitat', 'Ventes (sense IVA)': 'vendes'}, inplace=True)
 catalog_prices_2023_df.rename(columns={'Material (Referencia)': 'material', 'P.venda unit': 'preu_unitat'}, inplace=True)
 last_exit_entry_df.rename(columns={'Material (Referencia)': 'material', 'Data darrera sortida': 'data_sortida', 'Data darrera entrada': 'data_entrada'}, inplace=True)
 stock_31_12_2023_df.rename(columns={'Material (Referencia)': 'material', 'Stock total': 'stock', 'Valor total': 'valor_total', 'Cost unit': 'cost_unitat'}, inplace=True)
